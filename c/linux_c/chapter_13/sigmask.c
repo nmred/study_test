7e0a4ac0563ad91ffd0da51b8b6b76062b39909a
nmred <nmred_2008@126.com>
2013-10-18 23:13:50 -0400

    add signal set

---
+++ c/linux_c/chapter_13/sigmask.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void sigusr1_handler(int signo)
+{
+	printf("catch SIGUSR1\n");
+}
+
+int main(void)
+{
+	sigset_t set;
+	
+	if (signal(SIGUSR1, sigusr1_handler) == SIG_ERR) {
+		printf("cant't set handler for SIGUSR1");
+		exit(1);	
+	}
+
+	sigemptyset(&set);
+	sigaddset(&set, SIGUSR1);
+
+	if (sigprocmask(SIG_BLOCK, &set, NULL) == -1) {
+		perror("fail to set signal-mask");
+		exit(1);	
+	}
+
+	printf("SIGUSR1, is not available\n");
+
+	sleep(10);
+
+	if (sigprocmask(SIG_UNBLOCK, &set, NULL) == -1) {
+		perror("fail to set signal-mask");
+		exit(1);	
+	}
+
+	printf("SIGUSR1 is available now \n");
+
+	sleep(10);
+
+	return 0;
+}
---
+++ c/linux_c/chapter_13/sigset.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <signal.h>
+
+int main(void)
+{
+	sigset_t sig_set;
+	
+	sigemptyset(&sig_set);
+	sigaddset(&sig_set, SIGKILL);
+	
+	if ((sigismember(&sig_set, SIGKILL)) == 1) {
+		printf("SIGKILL has been set \n");	
+	} else {
+		printf("can't set signal set\n");	
+	}
+
+	return 0;
+}
