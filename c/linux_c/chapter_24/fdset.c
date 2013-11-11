efad1e499848c0db2c0b570d0f8b07256173884f
nmred <nmred_2008@126.com>
2013-11-11 14:48:30 +0800

    add

---
+++ c/linux_c/chapter_24/fdset.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/select.h>
+
+int main(void)
+{
+	fd_set fdset;
+	
+	FD_ZERO(&fdset);
+	
+	FD_SET(STDOUT_FILENO, &fdset);
+	
+	if (FD_ISSET(STDOUT_FILENO, &fdset) != 0) {
+		printf("stdout has been setted\n");	
+	} else {
+		printf("stdout has not been setted\n");	
+	}
+
+	FD_CLR(STDOUT_FILENO, &fdset);
+
+	if (FD_ISSET(STDOUT_FILENO, &fdset) != 0) {
+		printf("stdout has been setted\n");	
+	} else {
+		printf("stdout has not been setted\n");	
+	}
+
+	return 0;
+}
