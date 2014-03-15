a75554a58a51fea68552e2a426ae47e5c0a3a019
nmred <nmred_2008@126.com>
2014-03-15 23:32:15 +0800

    add Queue, HashMap

---
+++ javase/test_01/src/test07/NullInHashMap.java
@@ -0,0 +1,14 @@
+package test07;
+
+import java.util.HashMap;
+
+public class NullInHashMap {
+	public static void main(String[] args) {
+		HashMap hm1 = new HashMap();
+		hm1.put(null, null);
+		hm1.put(null, null);
+		hm1.put("a", null);
+		
+		System.out.println(hm1);
+	}
+}
