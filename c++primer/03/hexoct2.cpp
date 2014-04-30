ee832ced4e2d6667865ae2cbf718b40d2fbf7c36
nmred <nmred_2008@126.com>
2014-04-30 15:18:42 +0800

    add 03

---
+++ c++primer/03/hexoct2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int chest = 42;
+	int waist = 42;
+	int inseam = 42;
+	
+	cout << "Monsieur cuts a striking figure!" << endl;
+	cout << "chest = " << chest << "(decimal)" << endl;
+	cout << hex;
+	cout << "waist = " << waist << "(dexadecimal)" << endl;
+	cout << oct;
+	cout << "inseam = " << inseam << "(octal)" << endl;
+	return 0;	
+}
